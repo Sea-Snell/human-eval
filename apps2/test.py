d866658888be49cd23acdd40b1f99de3e2828a86
Sea-Snell <sea_snell@icloud.com>
2024-07-15 02:18:39 +0000

    merge

--- apps2/test.py
+++ apps2/test.py
@@ -136,6 +136,7 @@ def eval_and_save_problems(args):
         problems = load_dataset("codeparrot/apps", split=f"{args.split}[{start}:{args.stop_early}]", difficulties=[args.difficulty] if args.difficulty is not None else None)
     
     print('PROBLEMS CODES LENS LOG:', len(problems), len(codes), [args.difficulty])
+    raise NotImplementedError
 
     # main eval loop
     for index, problem in enumerate(tqdm(problems)):
